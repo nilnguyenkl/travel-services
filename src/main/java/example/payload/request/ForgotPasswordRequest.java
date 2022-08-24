8abe3253efa4ebe1f09958452f21e0dffe5171db
lnd8120 <62801046+LENHUTDUY-8120@users.noreply.github.com>
2022-08-24 22:44:25 +0700

    Update ForgotPasswordRequest.java

--- src/main/java/example/payload/request/ForgotPasswordRequest.java
+++ src/main/java/example/payload/request/ForgotPasswordRequest.java
@@ -4,9 +4,9 @@ public class ForgotPasswordRequest {
 	
 	private String email;
 	
-	public ForgotPasswordRequest(String email) {
-		this.email = email;
-	}
+// 	public ForgotPasswordRequest(String email) {
+// 		this.email = email;
+// 	} khong co viet cai nay!!!
 
 	public String getEmail() {
 		return email;
